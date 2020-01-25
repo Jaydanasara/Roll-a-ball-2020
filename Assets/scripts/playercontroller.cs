1bedd2527ff1279ead16a15d59db0c999e02d667
Joe watson <41978879+Jaydanasara@users.noreply.github.com>
2020-01-25 01:27:30 -0500

    final

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playercontroller : MonoBehaviour
 {
     public float speed;
-    
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+       SetCountText();
+       winText.text = "";
+
     }
 
     void FixedUpdate ()
@@ -28,7 +35,23 @@ public class playercontroller : MonoBehaviour
     {
         if (other.gameObject.CompareTag ("pick up"))
         {
+           GetComponent<AudioSource>().Play();
             other.gameObject.SetActive (false);
+            
+            count = count + 1;
+            SetCountText();
+
         }
     }
+      
+      void SetCountText()
+      {
+          countText.text ="Count:" + count.ToString();
+          if (count >=12){
+              winText.text="Game Over";
+              GetComponent<AudioSource>().Play();
+
+          }
+
+      }
 }
